2585588d8f66d057b30ac1b387ee18aaf5ecbb1c
saurabhdohaiya <84789753+saurabhdohaiya@users.noreply.github.com>
2021-10-28 11:47:57 +0530

    Update my.cpp

--- my.cpp
+++ my.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 int main(){
-  cout<<"My name is saurabh yadav">>:
+  cout<<"My name is Saurabh Yaday"<<endl:
+  cout<<"My name is Dev Aggrawal"<<endl:
+
   return 0;
 }
