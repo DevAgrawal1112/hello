d8efd2fed001d9309a26b11a8f880f347d3ef4f7
cosmicsaurabhyadav <77224668+cosmicsaurabhyadav@users.noreply.github.com>
2021-10-28 11:30:04 +0530

    Create q2.java

---
+++ q2.java
@@ -0,0 +1,12 @@
+final class Box {
+    static void display(int data) {
+        System.out.println(data);
+    }
+    static int j = 30;
+  
+public static void main(String[] args)
+    {
+        j = 36;
+        display(j);
+    }
+}
